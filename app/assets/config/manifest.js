8c80bece9c2c847695a45f3a84a1221526ae885b
unknown <vic.valesca@gmail.com>
2024-11-01 17:12:06 -0300

    devise&navb

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,16 @@
+//= link_tree ../images
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link_tree ../builds
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
+
+
+
+
+
+
+
+
+
