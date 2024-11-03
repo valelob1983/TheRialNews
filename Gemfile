26f7338507d18813a88a374d09b5649aead473c0
unknown <vic.valesca@gmail.com>
2024-11-03 01:48:02 -0300

    recuperacontraseña

--- Gemfile
+++ Gemfile
@@ -71,3 +71,5 @@ gem "devise", "~> 4.9"
 gem "cssbundling-rails", "~> 1.4"
 
 gem "jsbundling-rails", "~> 1.3"
+
+gem "figaro", "~> 1.2"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,11 +35,23 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = false 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+ 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV["gmail_email"],
+    password:             ENV["gmail_password"],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
+  
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
